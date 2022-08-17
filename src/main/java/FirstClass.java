32af25a74a3e5120efd47d83d66fe941bd9f6aee
unknown <nahid.student@wust.edu>
2022-08-17 19:07:12 -0400

    first commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,4 +2,21 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int a = 80;
+        int b = 20;
+
+        if (a < b) {
+            System.out.println(a+ " " + "grater than"+ " " +b);
+            }
+        else{
+            System.out.println(b+ " " + "grater than" + " " +a );
+        }
+        }
+
+
+
+
+
+
     }
