cbb7adfdc952328db8d2c003f86974392ada9e08
unknown <nahid.student@wust.edu>
2022-08-17 18:50:02 -0400

    first commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.firstproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.firstproject to javafx.fxml;
-    exports com.example.firstproject;
-}
